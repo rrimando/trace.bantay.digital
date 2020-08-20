b73ad22dd047b0538a1a5b68c579d3224dbce919
Zan Rouck Gamad <zanrouck.gamad@gmail.com>
2020-08-20 16:05:48 +0800

    Trace Bantay Digital

--- wyvern/settings.py
+++ wyvern/settings.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.sites",
     # QR Code,
-    "qrcode",
+    "qr_code",
     # Optimization
     "compressor",
     # User Agents - Detect Mobile
