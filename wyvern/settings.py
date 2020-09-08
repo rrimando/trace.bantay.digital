ac56277781feb8038057485fc888ec4fe14cbf77
Rohan <rohan.rimando@gmail.com>
2020-09-08 10:07:23 +0800

    Wyvern: QR Code fix

--- wyvern/settings.py
+++ wyvern/settings.py
@@ -14,6 +14,8 @@ import os
 import socket
 import configparser
 
+from qr_code.qrcode import constants
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
