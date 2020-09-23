602ad82335f0d9a2f987c8e38b92454801b7ff43
zanrouck <37526993+zanrouck@users.noreply.github.com>
2020-09-23 14:56:36 +0800

    Update settings.py

--- wyvern/settings.py
+++ wyvern/settings.py
@@ -50,7 +50,7 @@ ALLOWED_HOSTS = config.get("hosts", "allowed").split("\n")
 
 INSTALLED_APPS = [
     "wyvernuser",
-    "django.contrib.admin",
+    #"django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
